31e655fa974dbe8c37e38ad274135364c805d917
Pavel Korolev <dev@borodust.org>
2018-05-09 00:59:14 +0300

    Don't load trivial-gamekit when installing

--- scripts/install-dist.lisp
+++ scripts/install-dist.lisp
@@ -1,4 +1,3 @@
 (ql-dist:install-dist "http://bodge.borodust.org/dist/org.borodust.bodge.txt"
                       :prompt nil :replace t)
 (ql:update-all-dists :prompt nil)
-(ql:quickload :trivial-gamekit/distribution)
